fac0b49b461336464d6773f4c07fb71b52e168b2
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-19 14:24:46 +0000

    added upper threshold

--- lib/soundwave.rb
+++ lib/soundwave.rb
@@ -1,6 +1,8 @@
 def band_pass_filter(soundwave)
     if soundwave[0] < 40
         return [40]
+        elsif soundwave[0] > 1000
+        return [1000]
     else
         return soundwave
     end
--- spec/soundwave_spec.rb
+++ spec/soundwave_spec.rb
@@ -1,7 +1,7 @@
 require 'soundwave'
 
 describe 'soundwave' do
-    context 'If there is no band pass filterarguments' do
+    context 'If there is no band pass filter arguments' do
         it 'should return with the soundwave' do
             expect(band_pass_filter([50])).to eq([50])
         end
@@ -9,6 +9,11 @@ describe 'soundwave' do
     context 'If the frequency in the soundwave is below the lower threshold' do
         it 'should replace the frequency with the lower threshold value' do
             expect(band_pass_filter([30])).to eq([40])
+        end
+    end
+    context 'If the frequency in the soundwave is above the upper threshold' do
+        it 'should replace the frequency with the upper threshold value' do
+            expect(band_pass_filter([1050])).to eq([1000])
         end
     end
 end
