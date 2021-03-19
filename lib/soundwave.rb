ec0cd29fdf6bc05e4e1eaf66b6ccb57dd85325fe
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-19 14:21:59 +0000

    added lower threshold

--- lib/soundwave.rb
+++ lib/soundwave.rb
@@ -1,3 +1,7 @@
 def band_pass_filter(soundwave)
-    soundwave
+    if soundwave[0] < 40
+        return [40]
+    else
+        return soundwave
+    end
 end
--- spec/soundwave_spec.rb
+++ spec/soundwave_spec.rb
@@ -6,4 +6,9 @@ describe 'soundwave' do
             expect(band_pass_filter([50])).to eq([50])
         end
     end
+    context 'If the frequency in the soundwave is below the lower threshold' do
+        it 'should replace the frequency with the lower threshold value' do
+            expect(band_pass_filter([30])).to eq([40])
+        end
+    end
 end
